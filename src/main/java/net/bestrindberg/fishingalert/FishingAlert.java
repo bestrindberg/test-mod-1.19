fe27435eafc9180cb1a2b4bde6fe95cd2d3c2502
bestrindberg <bestrindberg@gmail.com>
2022-07-25 13:57:54 +0200

    Initial

---
+++ src/main/java/net/bestrindberg/fishingalert/FishingAlert.java
@@ -0,0 +1,15 @@
+package net.bestrindberg.fishingalert;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FishingAlert implements ModInitializer {
+	public static final String MOD_ID = "fishingalert";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bestrindberg/fishingalert/FishingAlertClient.java
@@ -0,0 +1,10 @@
+package net.bestrindberg.fishingalert;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishingAlertClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
